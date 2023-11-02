6681833797a9bbb0827599b6fc03133a4ad5078f
p. schuhmacher <contact@pschuhmacher.com>
2023-11-02 17:33:12 +0100

    remplace mysql par mysql2

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@
  * Export et test de la connexion à la base de données MySQL
  */
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
     host: 'db',
@@ -13,10 +13,11 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+connection.query('SELECT 1 + 2 AS solution', (err, rows, fields) => {
     if (err) throw err
     console.log('The solution is: ', rows[0].solution)
 })
+
 
 // connection.end()
 module.exports = connection
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,14 +5,21 @@ var connection = require('../db')
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
 
-  connection.query('SELECT * FROM User;',  (error, rows, fields) => {
-    const users = rows.map(element => {
-      return {
-        firstName: element.first_name
-      }
-    });
-    res.send(users);
-  })
+
+  connection.query(
+    'SELECT * FROM `User` WHERE `first_name` = ?',
+    ['Eve'],
+    function (err, rows, fields) {
+      const users = rows.map(element => {
+        return {
+          firstName: element.first_name
+        }
+      });
+      res.send(users);
+    }
+  );
+
+
 });
 
 module.exports = router;
