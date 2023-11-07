2ca008928ea531f35f572d3a552d10a0f971c89b
p. schuhmacher <contact@pschuhmacher.com>
2023-11-07 11:40:38 +0100

    fix import fichier inexistant suite au nettoyage

--- api/swagger.js
+++ api/swagger.js
@@ -2,7 +2,7 @@ const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
 
-const endpointsFiles = ['./routes/index.js', './routes/users.js']
+const endpointsFiles = ['./routes/index.js']
 
 const doc = {
     info: {
